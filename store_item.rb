195e9b56f3c8f05c3222b9821b3bdc3715df6360
dylan <dylan.sarikas@gmail.com>
2022-01-05 12:32:17 -0600

    Adds store with hashes

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+blobs = {price: 13, smell: "poor", color: "puke green"}
+globs = {price: 12, smell: "okay i guess", color: "can't really describe it"}
+slobs = {:price => 14, :smell => "wonderful", :color => "pretty in pink"}
+
+p blobs
+puts globs
+p slobs
+
+store = [blobs, globs, slobs]
+#p store[0][price:]
+p store[0][:price]
