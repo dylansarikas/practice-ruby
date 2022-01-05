2e3fded59362865f884596285cdb4f22b220722e
dylan <dylan.sarikas@gmail.com>
2022-01-05 13:54:02 -0600

    changes layout from hashes to class

--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,62 @@
-blobs = {price: 13, smell: "poor", color: "puke green"}
-globs = {price: 12, smell: "okay i guess", color: "can't really describe it"}
-slobs = {:price => 14, :smell => "wonderful", :color => "pretty in pink"}
+# blobs = {price: 13, smell: "poor", color: "puke green"}
+# globs = {price: 12, smell: "okay i guess", color: "can't really describe it"}
+# slobs = {:price => 14, :smell => "wonderful", :color => "pretty in pink"}
 
-p blobs
-puts globs
-p slobs
+# p blobs
+# puts globs
+# p slobs
 
-store = [blobs, globs, slobs]
-p store[0][:price]
-p store [0][:color]
+# store = [blobs, globs, slobs]
+# p store[0][:price]
+# p store [0][:color]
+
+class StoreFront
+  def initialize(price_input, smell_input, color_input)
+    @price = price_input
+    @smell = smell_input
+    @color = color_input
+  end
+
+  def price
+    @price
+  end
+
+  def smell
+    @smell
+  end
+
+  def color
+    @color
+  end
+
+  def price=(input)
+    @price = input
+  end
+
+  def smell=(input)
+    @smell = input
+  end
+
+  def color=(input)
+    @color = input
+  end
+
+  def print_out
+    puts "This costs #{price} schmackos, smells #{smell}, and looks like #{color}"
+  end
+
+
+end
+
+blobs = StoreFront.new(13, "poor", "puke green")
+globs = StoreFront.new(12, "okay i guess", "can't really describe it")
+slobs = StoreFront.new(14, "wonderful", "pretty in pink")
+blobs.print_out
+puts globs.price
+puts globs.smell
+puts slobs.color
+puts " "
+blobs.price=(1414)
+blobs.smell=("awfully poor")
+blobs.color=("noxious vomit green")
+blobs.print_out
